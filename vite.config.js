afce87af8a66d2eda9bc4367d9fd1716b0ecd950
chenchiaoyu <117254302+chenchiaoyu@users.noreply.github.com>
2026-06-25 15:23:51 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // 重要：把 'fortune-timeline' 換成你的 GitHub repo 名稱
 export default defineConfig({
   plugins: [react()],
-  base: '/fortune-timeline/',
+  base: '/fortuneteller/',
 })
